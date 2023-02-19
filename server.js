bd30df568c3ee8c87e2aa3238359973225969d57
javarip <kane.swartz@myport.ac.uk>
2023-02-19 12:07:11 +0000

    python to node request

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = express()
 // ports should be taken from env vars but hardcoded for simplicity
 const port = 3000
 
-app.get('/', async (_req, res) => {
-  // http:// required
-  // 127.0.0.1 works but localhost does not
-
-  const pres = await fetch('http://127.0.0.1:5000/')
+app.get('/ask_python', async (_req, res) => {
+  const pres = await fetch('http://localhost:5000/')
   res.send(await pres.text())
 })
 
+app.get('/', async(_req, res) => {
+  res.send('hello from node server\n')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on ${port}`)
 })
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def function():
-    return 'hello from python server\n'
+def reply():
+  return 'hello from python server\n'
+
+@app.route('/ask_node')
+def ask_node():
+  # http:// required
+  # 127.0.0.1 works but localhost does not
+
+  return requests.get('http://127.0.0.1:3000/').content
 
 if __name__ == '__main__':
-   app.run()
+  app.run()
